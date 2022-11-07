69276a6f211ed834f1edbb9a316cda703b1e228d
Vic Ding <qiqing.ding@di.huc.knaw.nl>
2022-11-07 22:06:08 +0100

    fix start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 scp .env pure3d.dev:/tmp/
-ssh pure3d.dev 'sudo mv /tmp/.env /tmp/app/.env && sudo mv /tmp/client_secrets.json /tmp/app/src/pure3d/control/ && if [ -d /tmp/app ];then cd /tmp/app; source .env; sudo git checkout ${gitbranch}; sudo git pull; else sudo git clone -b ${gitbranch} ${gitlocation} /tmp/app; cd /tmp/app; fi'
+
+ssh pure3d.dev 'sudo source /tmp/.env && sudo mv /tmp/.env /tmp/app/.env && sudo mv /tmp/client_secrets.json /tmp/app/src/pure3d/control/ && if [ -d /tmp/app ];then cd /tmp/app; source .env; sudo git checkout ${gitbranch}; sudo git pull; else sudo git clone -b ${gitbranch} ${gitlocation} /tmp/app; cd /tmp/app; fi'
 scp src/pure3d/control/client_secrets.json pure3d.dev:/tmp/
 if [ "$1" == "restart-only" ];then
   ssh pure3d.dev 'cd /tmp/app && sudo ./restart.sh'
