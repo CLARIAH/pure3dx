cd3df7005fa4eb01c3ba6ec10df3d4cfc70e9d59
Vic Ding <qiqing.ding@di.huc.knaw.nl>
2022-11-07 21:55:19 +0100

    scp env first

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-ssh pure3d.dev 'if [ -d /tmp/app ];then cd /tmp/app; source .env; sudo git checkout ${gitbranch}; sudo git pull; else sudo git clone -b ${gitbranch} ${gitlocation} /tmp/app; cd /tmp/app; fi'
 scp .env pure3d.dev:/tmp/
+ssh pure3d.dev 'if [ -d /tmp/app ];then cd /tmp/app; source .env; sudo git checkout ${gitbranch}; sudo git pull; else sudo git clone -b ${gitbranch} ${gitlocation} /tmp/app; cd /tmp/app; fi'
 scp src/pure3d/control/client_secrets.json pure3d.dev:/tmp/
 if [ "$1" == "restart-only" ];then
   ssh pure3d.dev 'sudo mv /tmp/.env /tmp/app/.env && sudo mv /tmp/client_secrets.json /tmp/app/src/pure3d/control/ && cd /tmp/app && sudo ./restart.sh'
