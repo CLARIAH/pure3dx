0a17f97cf1951568f0ae56c715d645759ecd650d
Vic Ding <qiqing.ding@di.huc.knaw.nl>
2022-11-07 21:53:18 +0100

    revise start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 ssh pure3d.dev 'if [ -d /tmp/app ];then cd /tmp/app; source .env; sudo git checkout ${gitbranch}; sudo git pull; else sudo git clone -b ${gitbranch} ${gitlocation} /tmp/app; cd /tmp/app; fi'
-scp .env pure3d.dev:/tmp
+scp .env pure3d.dev:/tmp/
+scp src/pure3d/control/client_secrets.json pure3d.dev:/tmp/
 if [ "$1" == "restart-only" ];then
-  ssh pure3d.dev 'sudo mv /tmp/.env /tmp/app/.env && cd /tmp/app && sudo ./restart.sh'
+  ssh pure3d.dev 'sudo mv /tmp/.env /tmp/app/.env && sudo mv /tmp/client_secrets.json /tmp/app/src/pure3d/control/ && cd /tmp/app && sudo ./restart.sh'
 else
-  ssh pure3d.dev 'sudo mv /tmp/.env /tmp/app/.env && cd /tmp/app && sudo ./build.sh && sudo ./restart.sh'
+  ssh pure3d.dev 'sudo mv /tmp/.env /tmp/app/.env && sudo mv /tmp/client_secrets.json /tmp/app/src/pure3d/control/ && cd /tmp/app && sudo ./build.sh && sudo ./restart.sh'
 fi
