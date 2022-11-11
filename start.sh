2fffac2df0636f8fd76aa9596e58990df07240d8
Vic Ding <qiqing.ding@di.huc.knaw.nl>
2022-11-11 10:31:46 +0100

    run provision

--- start.sh
+++ start.sh
@@ -3,6 +3,8 @@
 scp .env pure3d.dev:/tmp/
 scp src/pure3d/control/client_secrets.json pure3d.dev:/tmp/
 ssh pure3d.dev 'source /tmp/.env && if [ -d /tmp/app ];then cd /tmp/app; source .env; sudo git checkout ${gitbranch}; sudo git pull; else sudo git clone -b ${gitbranch} ${gitlocation} /tmp/app; cd /tmp/app; fi && sudo mv /tmp/.env /tmp/app/.env && sudo mv /tmp/client_secrets.json /tmp/app/src/pure3d/control/'
+ssh pure3d.dev 'sudo git clone https://github.com/CLARIAH/pure3d-data.git /tmp/pure3d-data && cd /tmp/app && sudo ./provision.sh'
+
 if [ "$1" == "restart-only" ];then
   ssh pure3d.dev 'cd /tmp/app && sudo ./restart.sh'
 else
