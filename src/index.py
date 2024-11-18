a580f48e9bc103382b4049c6bb18a5a22b0824ea
Dirk Roorda <dirk.roorda@di.huc.knaw.nl>
2024-11-18 18:42:18 +0100

    added comment on importing python modules

--- src/index.py
+++ src/index.py
@@ -1,6 +1,12 @@
 import os
 from control.webdavapp import appFactory
 
+# NB: it is intentionally from control.webdavapp and not from .control.webdavapp
+# Otherwise, the deployment over gunicorn fails with a relative import error message.
+# NB: there is no __init__.py at the toplevel of src.
+# Otherwise, the deployment via the flask dev server fails with an import error.
+
+
 flaskAppName = os.environ.get("FLASK_APP", None)
 
 if flaskAppName:
